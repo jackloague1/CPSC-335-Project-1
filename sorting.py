b531d6c704029b6bee2d279598b96ef64fe4c44b
Gennaquach <60235199+Gennaquach@users.noreply.github.com>
2022-02-17 23:23:05 -0800

    Add files via upload

---
+++ sorting.py
@@ -0,0 +1,54 @@
+def sort(lists):
+    sorting = True
+    m = 0
+    moves = 0
+    while moves > 0 or sorting:
+        sorting = False
+        moves = 0
+        # Iterating through the list from left to right.
+        for i in range(0, len(lists)-1):
+            if lists[i + 1] > lists[i]:
+                temp = lists[i]
+                lists[i] = lists[i + 1]
+                lists[i + 1] = temp
+                moves += 1   
+        m += moves
+        # Iterating through the list from right to left.
+        for i in range(len(lists)+1 , 0):
+            if lists[i] > lists[i - 1]:
+                temp = lists[i]
+                lists[i] = lists[i - 1]
+                lists[i - 1] = temp
+                moves += 1
+        m += moves
+        # Checking if the list is sorted.
+        for i in range(0, len(lists)):
+            if lists[i] != 0:
+                sorting == False
+
+            else:
+                 sorting == True
+    # Converting list of integers to strings to make it look better.           
+    new_list = [str(l) for l in lists]    
+    
+    print(' '.join(new_list) )
+    print("The number of swaps taken:")
+    return m
+
+# Initializing list of disk.
+input_n = input("Please enter a positive integer n for the amount of n light and n dark disks: ")
+# Checking if the input is a positive integer.
+if input_n.strip().isdigit():
+    n = int(input_n)
+    lists = [0, 1] * n
+    string_list = [str(s) for s in lists]
+    print("Let 0 be light and 1 be dark.")
+    print("The disks before sorting when n is", n,":")
+    print(' '.join(string_list) )
+    print("The disks after sorting:")
+    print(sort(lists))
+    
+# Ends program if the input is not a positive integer. 
+else:
+    print("The input is not a positive integer. Please try again.")
+
